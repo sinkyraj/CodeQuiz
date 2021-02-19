576a5b11554cdb258c6036165395ee30db4931c2
Shweta Sharma <dostind@gmail.com>
2021-02-18 21:05:26 -0800

    code

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+//Quiz questions, their options and answers
+
+var quizQuestions = [
+  {
+    questions: "What's the biggest animal in the world?",
+    options: ["Lion", "Ziraffe", "Elephant", "The blue whale"],
+    answer: "The blue whale"
+  },
+  {
+    questions: "Which planet is closest to the sun?",
+    options: ["Neptune", "Mercury", "Mars", "Earth"],
+    answer: "Mercury"
+  },
+  {
+    questions: "How many elements are there in the periodic table?",
+    options: ["18 elements", "110 elements", "90 elements", "118 elements"],
+    answer: "118 elements"
+  },
+  {
+    questions: "What fruit takes the scientific name Mangifera indica?",
+    options: ["Strawberry", "Mango", "Gauva", "Banana"],
+    answer: "Mango"
+  },
+  {
+    questions: "What is the largest country in the world?",
+    options: ["Russia", "Brazil", "India", "China"],
+    answer: "Russia"
+  },
+
+
+];
+
